f99ad7a498f03fb031af046c514ce4c86fb8c79a
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-17 18:46:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,11 @@ def prime?(num)
   if num <2
     return false
   end
-  (2..num).each do |i|
+  (2...num).each do |i|
+    
     if num%i==0
+      return true
+    elsif num ==2
       return true
     end
   end
